215a43ecf69c745c48931a46fc1fbf50f869935c
damianruta <damian.ruta@hotmail.com>
2024-01-03 18:12:13 +0100

    Initial Commit

---
+++ Dola/DolaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DolaApp.swift
+//  Dola
+//
+//  Created by Damian Ruta on 03/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct DolaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
